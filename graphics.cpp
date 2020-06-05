65addd638b56aab9157f626aa48aa187b8f9f84f
Youssef Hussien <youssef12468@gmail.com>
2020-06-05 12:55:35 +0200

    Create graphics function

---
+++ graphics.cpp
@@ -0,0 +1,180 @@
+#include "Graphics.h"
+#include <iostream>
+using namespace std;
+//HANDLE hConsole;
+//char fill_char;
+
+void initConsole()
+{
+//    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+//    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    //HWND hWnd = GetConsoleWindow();
+    setConsoleSize(130,35);
+    SetConsoleTitle("E-Commerce Management System");
+//    MoveWindow(hWnd,50,50,50,50,TRUE);
+//    ShowWindow(GetConsoleWindow(),SW_SHOWMAXIMIZED);
+    color(LIGHT_GRAY);
+	//clearScreen();
+}
+
+void setConsoleSize(short width, short height)
+{
+    _COORD coord={width,height};
+
+    _SMALL_RECT Rect;
+    Rect.Top=0;
+    Rect.Left=0;
+    Rect.Bottom=height-1;
+    Rect.Right=width-1;
+
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleScreenBufferSize(Handle, coord);
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);
+}
+
+void editCursor(bool visible,float sizeCursor)
+{
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize=sizeCursor;
+   info.bVisible=visible;
+   SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&info);
+}
+
+void color(Colors foreground, Colors background)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),(WORD)((background << 4) | foreground));
+}
+
+void setCursor(int x, int y)
+{
+	COORD cursorPosition;
+	cursorPosition.X=x;
+	cursorPosition.Y=y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),cursorPosition);
+}
+
+void clearScreen()
+{
+	setCursor(0, 24);
+	for (int j = 0; j < 25; j++)
+		_putch('\n');
+	setCursor(0, 0);
+}
+
+void wait(int milliseconds)
+{
+	Sleep(milliseconds);
+}
+
+void clearLine(int length)
+{
+    while(length--)
+    {
+        cout<<" ";
+    }
+}
+
+void drawRectangle(int x, int y, int width, int height,int style)
+{
+    height+=1;
+    width+=2;
+    char topLeft,topRight,bottomLeft,bottomRight,verticalLine,horizontalLine;
+    switch(style)
+    {
+        case 1:
+            topLeft=char(218);
+            topRight=char(191);
+            bottomLeft=char(192);
+            bottomRight=char(217);
+            verticalLine=char(196);
+            horizontalLine=char(179);
+            break;
+        case 2:
+            topLeft=char(201);
+            topRight=char(187);
+            bottomLeft=char(200);
+            bottomRight=char(188);
+            verticalLine=char(205);
+            horizontalLine=char(186);
+            break;
+    }
+
+    setCursor(x,y);
+    cout<<topLeft;
+	for(int i=1; i<width-1 ; i++)
+        cout<<verticalLine;
+    cout<<topRight;
+
+	setCursor(x,y+height);
+    cout<<bottomLeft;
+	for(int i=1 ; i<width-1 ; i++)
+        cout<<verticalLine;
+    cout<<bottomRight;
+
+    setCursor(x,y+1);
+	for (int i=y+1 ; i<y+height ; i++)
+	{
+		setCursor(x,i);
+		cout<<horizontalLine;
+		setCursor(x+width-1,i);
+		cout<<horizontalLine;
+	}
+}
+
+void drawCircle(int xC, int yC, int radius)
+{
+	double theta, increment, xF, pi=3.14159;
+	int x, xN, yN;
+
+	increment=0.8/static_cast<double>(radius);
+	for (theta=0; theta<=pi/2; theta += increment)
+	{
+		xF=radius * cos(theta);
+		xN=static_cast<int>(xF*2/1);
+
+		yN=static_cast<int>(radius*sin(theta)+0.5);
+		x=xC-xN;
+		while(x<=xC+xN)
+		{
+			setCursor(x, yC-yN); cout<<char(191)<<endl;
+			setCursor(x++, yC+yN); cout<<char(191)<<endl;
+		}
+	}
+}
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+	double deltaX=x1-x2, deltaY=y1-y2;
+	if(fabs(deltaX)>fabs(deltaY))
+	{
+		double slope=deltaY/deltaX;
+		for (int i=x1; i<=x2; i++)
+        {
+            int z=static_cast<int>(y1+slope*(i-x1));
+            setCursor(i,z);  cout<<char(196);
+        }
+	}
+	else
+	{
+		double slope=deltaX/deltaY;
+		for (int i=y1; i<=y2; i++)
+        {
+            int z=static_cast<int>(x1+slope*(i-y1));
+            setCursor(z, i);
+            cout<<char(179);
+        }
+	}
+}
+
+//void setFillStyle(FillStyle fillStyle)
+//{
+//	switch (fillStyle)
+//	{
+//	case SOLID_FILL:    fillChar='\xB3'; break;
+//	case DARK_FILL:     fillChar='\xB0'; break;
+//	case MEDIUM_FILL:   fillChar='\xB1'; break;
+//	case LIGHT_FILL:    fillChar='\xB2'; break;
+//	case X_FILL:        fillChar='X'; break;
+//    case O_FILL:        fillChar='O'; break;
+//	}
+//}
---
+++ graphics.h
@@ -0,0 +1,76 @@
+#ifndef GRAPHICS_H_INCLUDED
+#define GRAPHICS_H_INCLUDED
+
+#include <Windows.h>
+#include <conio.h>
+#include <math.h>
+
+#define KEY_UP          72
+#define KEY_DOWN        80
+#define KEY_LEFT        75
+#define KEY_RIGHT       77
+#define KEY_ENTER       13
+
+#define KEY_HOME        71
+#define KEY_ESC        27
+#define KEY_CENTER      76
+#define KEY_END         79
+
+#define KEY_PAGEUP      73
+#define KEY_PAGEDOWN    81
+
+#define KEY_INSERT      82
+#define KEY_DELETE      83
+
+#define KEY_F1          59
+#define KEY_F2          60
+#define KEY_F3          61
+#define KEY_F4          62
+#define KEY_F5          63
+#define KEY_F6          64
+#define KEY_F7          65
+#define KEY_F8          66
+#define KEY_F9          67
+
+enum FillStyle {
+    SOLID_FILL,
+    X_FILL,
+    O_FILL,
+    LIGHT_FILL,
+    MEDIUM_FILL,
+    DARK_FILL
+};
+
+enum Colors {
+    BLACK=0,
+    DARK_BLUE,
+    DARK_GREEN,
+    DARK_CYAN,
+    DARK_RED,
+    DARK_MAGENTA,
+    BROWN,
+    LIGHT_GRAY,
+    DARK_GRAY,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    YELLOW,
+    WHITE
+};
+
+void initConsole();
+void setConsoleSize(short, short);
+void color(Colors foreground=LIGHT_GRAY, Colors background=BLACK);
+void setCursor(int x, int y);
+void editCursor(bool ,float sizeCursor=1);
+void clearScreen();
+void clearLine(int);
+void wait(int);
+void drawRectangle(int, int, int, int,int style=1);
+void drawCircle(int, int, int);
+void drawLine(int, int, int, int);
+void setFillStyle(FillStyle);
+
+#endif // GRAPHICS_H_INCLUDED
